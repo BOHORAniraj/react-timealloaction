98b33e5b63811d7e376eaaacea36e4bbfae5c1a5
bohoraniraj <nirajbohora1993@gmail.com>
2021-08-13 09:11:05 +1000

    product initialize

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Alert from 'react-bootstrap/Alert'
+
+
+
+function App() {
+  return (
+  <div> 
+    <Alert variant ="danger">hello world</Alert>
+  </div>
+  );  
+}
+
+export default App;
